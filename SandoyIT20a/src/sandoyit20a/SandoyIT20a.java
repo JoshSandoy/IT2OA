31efa6db6c4e933a3dce17e3323a1f6320c67dce
JoshSandoy <leeken851@gmail.com>
2025-09-04 15:22:53 +0800

    HelloWorld

---
+++ SandoyIT20a/src/sandoyit20a/SandoyIT20a.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sandoyit20a;
+
+/**
+ *
+ * @author PC-6
+ */
+public class SandoyIT20a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+    
+}
